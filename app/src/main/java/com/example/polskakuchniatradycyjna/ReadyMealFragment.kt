1bba2f36ccc7e02a36f754ec9e5b0ea1f822078d
Filip <filip.flicinski@cyfroweszkoly.pl>
2025-12-17 20:40:47 +0100

    add actions and make moving to the summary fragment from meals fragments

--- app/src/main/java/com/example/polskakuchniatradycyjna/ReadyMealFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna/ReadyMealFragment.kt
@@ -5,19 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.polskakuchniatradycyjna.databinding.FragmentMenuChoiceBinding
+import com.example.polskakuchniatradycyjna.databinding.FragmentReadyMealBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ReadyMealFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ReadyMealFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var _binding: FragmentReadyMealBinding?=null
+    private val binding get()=_binding!!
+
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,24 +29,25 @@ class ReadyMealFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ready_meal, container, false)
+        _binding= FragmentReadyMealBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.readyMealButton.setOnClickListener {
+            findNavController().navigate(R.id.action_readyMealFragment_to_summaryFragment)
+        }
+    }
+
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReadyMealFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ReadyMealFragment().apply {
