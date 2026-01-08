971e2a6f70d438d849483a59dc128056cf3faa4b
filip <filip.flicinski@cyfroweszkoly.pl>
2026-01-08 08:47:10 +0100

    finish application track

--- app/src/main/java/com/example/polskakuchniatradycyjna/SummaryFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna/SummaryFragment.kt
@@ -5,19 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.polskakuchniatradycyjna.databinding.FragmentCustomMealBinding
+import com.example.polskakuchniatradycyjna.databinding.FragmentSummaryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SummaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SummaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var _binding: FragmentSummaryBinding?=null
+    private val binding get()=_binding!!
+
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,20 +33,19 @@ class SummaryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false)
+        _binding= FragmentSummaryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.summaryButton.setOnClickListener{
+            findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
+        }
+    }
+
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SummaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SummaryFragment().apply {
